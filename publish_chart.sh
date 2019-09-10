c628c595a7cefe75624eb081cd391669ace4890c
Evgeni Gordeev <egordeev@conserviscorp.com>
2019-09-10 11:12:26 -0500

    add index.yaml

--- publish_chart.sh
+++ publish_chart.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -e
 helm package kube-asg-node-drainer
+mv kube-asg-node-drainer-1.0.0.tgz kube-asg-node-drainer
 helm repo index kube-asg-node-drainer/ --url https://conservis.github.io/kube-asg-node-drainer/
