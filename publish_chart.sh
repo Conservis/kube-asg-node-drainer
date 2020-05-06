a88d484bc33faa9f18d38e857267ab2fdbdd1763
Evgeni Gordeev <egordeev@conserviscorp.com>
2020-05-06 10:53:34 -0500

    kubectl 1.16 support

--- publish_chart.sh
+++ publish_chart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 helm package kube-asg-node-drainer
-helm repo index ./ --url https://conservis.github.io/kube-asg-node-drainer/
+helm repo index --url https://conservis.github.io/kube-asg-node-drainer/ --merge index.yaml .
